51c76bb6b041d9c1cab7202f48b3e71fe89588b0
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-01-28 13:53:07 -0300

    update SearchBarComponent

---
+++ src/components/SearchBarComponent/SearchBarComponent.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import SearchBarComponent from "./SearchBarComponent.vue";
+
+describe("HelloWorld", () => {
+  it("renders properly", () => {
+    const wrapper = mount(SearchBarComponent, {
+      props: { msg: "Hello Vitest" },
+    });
+    expect(wrapper.text()).toContain("Hello Vitest");
+  });
+});
