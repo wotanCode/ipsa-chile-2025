50dd53123f2ca63ffefe403b278314b106cf1127
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-01-28 21:25:06 -0300

    wip header compnent

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -6,7 +6,36 @@
 </script>
 
 <template>
-  <div>Header Component</div>
+  <div class="headerContainer">
+    <h2 class="title">IPSA, Chile</h2>
+
+    <p>Indice:</p>
+    <hr />
+
+    <div class="valuesContainer">
+      <p>Valor actual: arriba 12</p>
+      <p>Var. % Actual -0.12%</p>
+      <p>Var. Puntos Actual: -23,23</p>
+    </div>
+
+    <hr />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+  margin: 0;
+}
+.headerContainer {
+  display: flex;
+  flex-direction: column;
+}
+
+.valuesContainer {
+  display: flex;
+}
+
+.title {
+  color: var(--vt-c-white-mute);
+}
+</style>
--- src/layout/BaseLayout.vue
+++ src/layout/BaseLayout.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
 import SearchBarComponent from "@/components/SearchBarComponent/SearchBarComponent.vue";
+import HeaderComponent from "@/components/HeaderComponent/HeaderComponent.vue";
 </script>
 
 <template>
   <div class="layoutContainer">
-    <header>
+    <header class="headerContainer">
       <SearchBarComponent />
-
-      <div>
-        <vk-button primary>Mi Botón</vk-button>
-        <vk-icon icon="heart"></vk-icon>
-      </div>
     </header>
 
-    <main></main>
+    <main>
+      <div>
+        <HeaderComponent />
+
+        <div>
+          <h2>RESUMEN</h2>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <style scoped>
 .layoutContainer {
+  display: flex;
+  flex-direction: column;
   max-width: 1280px;
   width: 100%;
   margin: 0 auto;
   padding: 2rem;
   font-weight: normal;
+  gap: 2rem;
 }
 </style>
