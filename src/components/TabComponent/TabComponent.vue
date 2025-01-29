f7a00669880d65aa940885d0dbc1d3316e1452c5
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-01-29 19:08:25 -0300

    update tab component

--- src/components/TabComponent/TabComponent.vue
+++ src/components/TabComponent/TabComponent.vue
@@ -10,44 +10,52 @@ const options = ["AGUASA", "ANDINAB", "BCI", "BSANTANDER", "CAP", "IPSA"];
 </script>
 
 <template>
-  <div class="tabs">
-    <button
-      v-for="(label, key) in options"
-      :key="key"
-      class="tab"
-      :class="{ active: selectedKey === label }"
-      @click="selectTab(label)"
-    >
-      {{ label }}
-    </button>
+  <div class="tabContainer">
+    <div class="tabs">
+      <button
+        v-for="(label, key) in options"
+        :key="key"
+        class="tab"
+        :class="{ active: selectedKey === label }"
+        @click="selectTab(label)"
+      >
+        {{ label }}
+      </button>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.tabContainer {
+  border-bottom: 2px solid #ccc;
+}
 .tabs {
   display: flex;
   gap: 10px;
 }
 
 .tab {
+  color: #f1f1f1;
   background: transparent;
-  border-bottom: 2px solid #ccc;
+  border: none;
+  outline: none;
+  appearance: none;
   padding: 10px 20px;
   font-size: 1rem;
   cursor: pointer;
   text-align: center;
-  /* border-radius: 5px; */
-  transition: background-color 0.3s, color 0.3s;
+  transition: border-bottom 0.3s, color 0.3s;
+  border-bottom: 2px solid transparent;
 }
 
-.tab:hover {
-  background-color: #007aff;
-  color: white;
+.tab:hover,
+.tab:focus {
+  border-bottom: 2px solid #ccc;
+  color: #fff;
 }
 
 .tab.active {
-  background-color: #007aff;
-  color: white;
-  font-weight: bold;
+  border-bottom: 2px solid #007aff;
+  color: #fff;
 }
 </style>
