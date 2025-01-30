9ad60626287647f358bcc841591f1c0ee0aaa861
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-01-30 13:22:42 -0300

    basic test e2e

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,42 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe("My First Test", () => {
-  it("visits the app root url", () => {
+describe("Test básico de componentes", () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.contains("h1", "You did it!");
+  });
+
+  it("Debería cargar el gráfico correctamente", () => {
+    cy.get("canvas").should("be.visible");
+
+    cy.get("p.uk-text-muted").should("not.exist"); // No debe existir el mensaje de "Cargando datos..."
+    cy.get("p.uk-text-danger").should("not.exist"); // No debe existir un mensaje de error
+  });
+
+  it("Debería cambiar el timeframe al hacer clic en un botón", () => {
+    cy.get(".btnTime").as("timeframeButton");
+
+    cy.get("@timeframeButton").should("have.length", 4);
+
+    cy.get("@timeframeButton").first().click();
+    cy.get("@timeframeButton").first().should("have.class", "uk-active");
+  });
+
+  it("Debería mostrar los valores correctamente en el encabezado", () => {
+    cy.get(".presentValue").should("exist");
+
+    cy.get("span.uk-text-success").should("exist");
+    cy.get("span.uk-text-danger").should("exist");
+  });
+
+  it("Debería mostrar los instrumentos correctamente en la tabla", () => {
+    cy.get("table tbody tr").should("have.length.greaterThan", 0);
+
+    cy.get("th").contains("↑↓Nombre").should("be.visible");
+    cy.get("th").contains("↑↓Último*").should("be.visible");
+    cy.get("th").contains("↑↓Monto").should("be.visible");
+  });
+
+  it("Debería permitir ordenar los datos de la tabla por nombre", () => {
+    cy.get("th").contains("↑↓Nombre").click();
+
+    cy.get("tr").first().contains("Instrumento A");
   });
 });
