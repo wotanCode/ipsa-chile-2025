5379c54cc911dde526bd1599ddd1f3356a17a071
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-01-30 10:09:49 -0300

    sorting

--- src/components/InstrumentListComponent/InstrumentListComponent.vue
+++ src/components/InstrumentListComponent/InstrumentListComponent.vue
@@ -1,15 +1,15 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useInstrumentStore } from "@/stores/instrumentsStore";
 import { storeToRefs } from "pinia";
 
 const store = useInstrumentStore();
 const { instrumentsData, isLoading, error } = storeToRefs(store);
 
-const splitData = (data) => {
-  const mid = Math.ceil(data.length / 2);
-  return [data.slice(0, mid), data.slice(mid)];
-};
+const sortConfig = ref({
+  field: "name",
+  direction: "asc",
+});
 
 const getClass = (value) => {
   return value < 0 ? "uk-text-danger" : "uk-text-success";
@@ -17,6 +17,42 @@ const getClass = (value) => {
 
 const formatValue = (value) => {
   return value ? value.toFixed(2) : "0.00";
+};
+
+const sortData = (field) => {
+  const direction =
+    sortConfig.value.field === field && sortConfig.value.direction === "asc"
+      ? "desc"
+      : "asc";
+  sortConfig.value = { field, direction };
+
+  instrumentsData.value.data.constituents.sort((a, b) => {
+    const aValue = a[field];
+    const bValue = b[field];
+
+    if (aValue < bValue) {
+      return sortConfig.value.direction === "asc" ? -1 : 1;
+    }
+    if (aValue > bValue) {
+      return sortConfig.value.direction === "asc" ? 1 : -1;
+    }
+    return 0;
+  });
+};
+
+const splitData = () => {
+  if (
+    !instrumentsData.value ||
+    !instrumentsData.value.data ||
+    !instrumentsData.value.data.constituents
+  ) {
+    return [[], []];
+  }
+  const mid = Math.ceil(instrumentsData.value.data.constituents.length / 2);
+  return [
+    instrumentsData.value.data.constituents.slice(0, mid),
+    instrumentsData.value.data.constituents.slice(mid),
+  ];
 };
 
 onMounted(() => {
@@ -34,23 +70,56 @@ onMounted(() => {
 
     <div v-if="!isLoading && !error">
       <div class="half-table">
-        <table v-if="instrumentsData">
+        <table v-if="instrumentsData && instrumentsData.data">
           <thead>
             <tr>
-              <th><strong>↑↓Nombre</strong></th>
-              <th><strong>↑↓Último*</strong></th>
-              <th><strong>↑↓Monto</strong></th>
-              <th><strong>↑↓Var Día</strong></th>
-              <th><strong>↑↓Var 30d**</strong></th>
-              <th><strong>↑↓Año Actual</strong></th>
-              <th><strong>↑↓12 Meses</strong></th>
+              <th @click="sortData('name')">
+                <strong>↑↓Nombre</strong>
+                <!-- <span v-if="sortConfig.field === 'name'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('lastPrice')">
+                <strong>↑↓Último*</strong>
+                <!-- <span v-if="sortConfig.field === 'lastPrice'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('volumeMoney')">
+                <strong>↑↓Monto</strong>
+                <!-- <span v-if="sortConfig.field === 'volumeMoney'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pctDay')">
+                <strong>↑↓Var Día</strong>
+                <!-- <span v-if="sortConfig.field === 'pctDay'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pct30D')">
+                <strong>↑↓Var 30d**</strong>
+                <!-- <span v-if="sortConfig.field === 'pct30D'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pctCY')">
+                <strong>↑↓Año Actual</strong>
+                <!-- <span v-if="sortConfig.field === 'pctCY'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pct1Y')">
+                <strong>↑↓12 Meses</strong>
+                <!-- <span v-if="sortConfig.field === 'pct1Y'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
             </tr>
           </thead>
           <tbody>
             <tr
-              v-for="instrument in splitData(
-                instrumentsData.data.constituents
-              )[0]"
+              v-for="instrument in splitData()[0]"
               :key="instrument.codeInstrument"
             >
               <td>
@@ -74,23 +143,56 @@ onMounted(() => {
           </tbody>
         </table>
 
-        <table v-if="instrumentsData">
+        <table v-if="instrumentsData && instrumentsData.data">
           <thead>
             <tr>
-              <th><strong>↑↓Nombre</strong></th>
-              <th><strong>↑↓Último*</strong></th>
-              <th><strong>↑↓Monto</strong></th>
-              <th><strong>↑↓Var Día</strong></th>
-              <th><strong>↑↓Var 30d**</strong></th>
-              <th><strong>↑↓Año Actual</strong></th>
-              <th><strong>↑↓12 Meses</strong></th>
+              <th @click="sortData('name')">
+                <strong>↑↓Nombre</strong>
+                <!-- <span v-if="sortConfig.field === 'name'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('lastPrice')">
+                <strong>↑↓Último*</strong>
+                <!-- <span v-if="sortConfig.field === 'lastPrice'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('volumeMoney')">
+                <strong>↑↓Monto</strong>
+                <!-- <span v-if="sortConfig.field === 'volumeMoney'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pctDay')">
+                <strong>↑↓Var Día</strong>
+                <!-- <span v-if="sortConfig.field === 'pctDay'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pct30D')">
+                <strong>↑↓Var 30d**</strong>
+                <!-- <span v-if="sortConfig.field === 'pct30D'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pctCY')">
+                <strong>↑↓Año Actual</strong>
+                <!-- <span v-if="sortConfig.field === 'pctCY'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
+              <th @click="sortData('pct1Y')">
+                <strong>↑↓12 Meses</strong>
+                <!-- <span v-if="sortConfig.field === 'pct1Y'">
+                  {{ sortConfig.direction === "asc" ? "↑" : "↓" }}
+                </span> -->
+              </th>
             </tr>
           </thead>
           <tbody>
             <tr
-              v-for="instrument in splitData(
-                instrumentsData.data.constituents
-              )[1]"
+              v-for="instrument in splitData()[1]"
               :key="instrument.codeInstrument"
             >
               <td>
