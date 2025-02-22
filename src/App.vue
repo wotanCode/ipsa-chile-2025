b2105ddcafa919fad47ef6c73b05405bcbf1e7bc
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-22 14:53:33 -0300

    feat: logo colors

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useDark } from '@vueuse/core'
+
 import NavBar from '@/UI/modules/NavBar/NavBar.vue'
+
+import alegraLogoBlack from '@/assets/logo-alegra-black.svg'
+import alegraLogoWhite from '@/assets/logo-alegra-white.svg'
+
+const isDark = useDark({ selector: 'html', attribute: 'class' })
+
+const logo = computed(() => (isDark.value ? alegraLogoWhite : alegraLogoBlack))
 </script>
+
 <template>
   <div class="lg:flex">
     <header
-      class="p-6 shadow-lg rounded-lg mb-6 lg:mb-0 lg:mr-4 lg:w-64 lg:flex-shrink-0 lg:max-h-screen lg:overflow-y-auto lg:overflow-x-hidden"
+      class="p-6 shadow-lg rounded-lg mb-6 lg:mb-0 lg:mr-4 lg:w-64 lg:flex-shrink-0 lg:max-h-screen lg:overflow-y-auto lg:overflow-x-hidden flex flex-col"
     >
-      <img
-        alt="Alegra logo"
-        class="mb-6 hidden dark:block"
-        src="@/assets/logo-alegra-white.svg"
-        width="125"
-        height="125"
-      />
-
-      <img
-        alt="Alegra logo"
-        class="mb-6 dark:hidden"
-        src="@/assets/logo-alegra-black.svg"
-        width="125"
-        height="125"
-      />
+      <img alt="Alegra logo" class="mb-6" :src="logo" width="125" height="125" />
 
       <NavBar />
     </header>
