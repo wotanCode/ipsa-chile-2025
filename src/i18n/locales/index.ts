bb27bcde7250fdbea0fa1e7c70e60c114c27a2d3
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-23 01:39:08 -0300

    feat: wip vendedores

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,11 +1,13 @@
 import esNavbar from '@/i18n/locales/es/navbar.json'
 import esHome from '@/i18n/locales/es/home.json'
+import esSellers from '@/i18n/locales/es/sellers.json'
 import esAbout from '@/i18n/locales/es/about.json'
 
 export const messages = {
   es: {
     navbar: esNavbar,
     home: esHome,
+    sellers: esSellers,
     about: esAbout,
   },
 }
---
+++ src/views/SellersView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import LayoutTemplate from '@/UI/layout/LayoutTemplate.vue'
+
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <LayoutTemplate :title="t('sellers.title')">
+    <div>vendedores</div>
+  </LayoutTemplate>
+</template>
