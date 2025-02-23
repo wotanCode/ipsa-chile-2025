54c5e220447aec3fc4e889d851552de6d9ade1c2
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-23 20:19:46 -0300

    feat: generic api fetching

---
+++ src/utils/apiFetch.ts
@@ -0,0 +1,27 @@
+export async function apiFetch(endpoint: string, options: RequestInit = {}) {
+  const API_URL = import.meta.env.VITE_ALEGRA_API_URL || ''
+  const TOKEN = import.meta.env.VITE_ALEGRA_API_TOKEN || ''
+
+  const defaultHeaders = new Headers({
+    'Content-Type': 'application/json',
+    Authorization: `Basic ${TOKEN}`,
+  })
+
+  const headers = options.headers ? new Headers(options.headers) : new Headers()
+  defaultHeaders.forEach((value, key) => {
+    if (!headers.has(key)) {
+      headers.set(key, value)
+    }
+  })
+
+  const config: RequestInit = {
+    ...options,
+    headers,
+  }
+
+  const response = await fetch(`${API_URL}${endpoint}`, config)
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}: ${response.statusText}`)
+  }
+  return response.json()
+}
