7f1a46934808bd66964c67f83748f81ca93c1bf1
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-23 01:58:36 -0300

    feat: NavButton test

--- src/UI/components/NavButton/NavButton.spec.ts
+++ src/UI/components/NavButton/NavButton.spec.ts
@@ -1,11 +1,59 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import { markRaw } from 'vue'
+import NavButton from '@/UI/components/NavButton/NavButton.vue'
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes: [{ path: '/', component: { template: 'Home' } }],
+})
+
+const DummyIcon = markRaw({
+  template: '<div />',
+})
+
+describe('NavButton.vue component', () => {
+  test('renders correctly with props', async () => {
+    router.push('/')
+    await router.isReady()
+
+    const wrapper = mount(NavButton, {
+      global: {
+        plugins: [router],
+      },
+      props: {
+        text: 'Go to Home',
+        icon: DummyIcon,
+        id: 'button-home',
+        url: '/',
+      },
+    })
+
+    const link = wrapper.find('a')
+    expect(link.exists()).toBe(true)
+    expect(link.attributes('href')).toBe('/')
+    expect(link.text()).toBe('Go to Home')
+    expect(wrapper.findComponent(DummyIcon).exists()).toBe(true)
+  })
+
+  test('navigates to the correct URL when clicked', async () => {
+    router.push('/other')
+    await router.isReady()
+
+    const wrapper = mount(NavButton, {
+      global: {
+        plugins: [router],
+      },
+      props: {
+        text: 'Go to Home',
+        icon: DummyIcon,
+        id: 'button-home',
+        url: '/',
+      },
+    })
+
+    await wrapper.find('a').trigger('click')
+    expect(router.currentRoute.value.path).toBe('/')
   })
 })
