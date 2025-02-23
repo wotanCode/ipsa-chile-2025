4c12cd5ad0142b818d3789f5d2a98df5d194be07
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-22 21:53:33 -0300

    wip: thewelcome

--- src/UI/components/TheWelcome/TheWelcome.vue
+++ src/UI/components/TheWelcome/TheWelcome.vue
@@ -1,14 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
 
 <template>
-  <h1>Imágenes del mundo</h1>
-  <p>
-    La empresa “Imágenes del mundo” ha realizado un concurso para sus vendedores, ¡una carrera para
-    encontrar las imágenes más interesantes de la web! La empresa “Imágenes del mundo” ha realizado
-    un concurso para sus vendedores, ¡una carrera para encontrar las imágenes más interesantes de la
-    web! La empresa “Imágenes del mundo” ha realizado un concurso para sus vendedores, ¡una carrera
-    para encontrar las imágenes más interesantes de la web! La empresa “Imágenes del mundo” ha
-    realizado un concurso para sus vendedores, ¡una carrera para encontrar las imágenes más
-    interesantes de la web!
-  </p>
+  <h1 class="text-4xl">{{ t('home.title') }}</h1>
+  <p>{{ t('home.primaryParagraph') }}</p>
+
+  <p>wip</p>
 </template>
