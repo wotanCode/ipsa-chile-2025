c0c7019771380b0c5b437140796d90941b62f5b6
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-22 23:35:14 -0300

    fix: heaer height

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import alegraLogoWhite from '@/assets/logo-alegra-white.svg'
 <template>
   <div class="lg:flex">
     <header
-      class="p-6 shadow-lg rounded-lg mb-6 lg:mb-0 lg:mr-4 lg:w-64 lg:flex-shrink-0 lg:max-h-screen lg:overflow-y-auto lg:overflow-x-hidden flex flex-col bg-[rgb(36,46,52)] dark:bg-neutral-50"
+      class="p-6 shadow-lg rounded-lg mb-6 lg:mb-0 lg:mr-4 lg:w-64 lg:flex-shrink-0 lg:max-h-screen lg:overflow-y-auto lg:overflow-x-hidden flex flex-col bg-[rgb(36,46,52)] dark:bg-neutral-50 self-start"
     >
       <img
         alt="Alegra logo"
