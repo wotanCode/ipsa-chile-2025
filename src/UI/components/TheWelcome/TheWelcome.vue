080de32ae867def4c70bd752a21a67529a943a75
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-22 23:21:23 -0300

    feat: i18n

--- src/UI/components/TheWelcome/TheWelcome.vue
+++ src/UI/components/TheWelcome/TheWelcome.vue
@@ -5,6 +5,21 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <p>{{ t('home.primaryParagraph') }}</p>
-  <p>wip</p>
+  <p class="border-b-2 border-gray-300/50 pb-2 italic text-gray-700/60 dark:text-gray-300/80">
+    {{ t('home.primaryParagraph') }}
+  </p>
+  <article class="flex flex-col gap-4 mt-4">
+    <p>{{ t('home.introText') }}</p>
+
+    <p>
+      <strong>{{ t('home.howItWorks') }}</strong>
+    </p>
+    <ul class="list-disc list-inside">
+      <li>{{ t('home.pointsList.challenge') }}</li>
+      <li>{{ t('home.pointsList.points') }}</li>
+      <li>{{ t('home.pointsList.prize') }}</li>
+    </ul>
+
+    <p>{{ t('home.raceConclusion') }}</p>
+  </article>
 </template>
