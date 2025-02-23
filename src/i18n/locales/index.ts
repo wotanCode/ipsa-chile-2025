dcb61ef35a156e650b33f0137736146f47c6fccf
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-22 22:24:43 -0300

    feat: add about base json

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,9 +1,11 @@
-import esNavbar from './es/navbar.json'
-import esHome from './es/home.json'
+import esNavbar from '@/i18n/locales/es/navbar.json'
+import esHome from '@/i18n/locales/es/home.json'
+import esAbout from '@/i18n/locales/es/about.json'
 
 export const messages = {
   es: {
     navbar: esNavbar,
     home: esHome,
+    about: esAbout,
   },
 }
