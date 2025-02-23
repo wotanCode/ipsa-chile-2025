0d16d2dcdfb861fcc26c9f9509f1aa7ee9b431e4
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-23 18:24:43 -0300

    fix: unnecesary import

--- src/UI/components/BaseModal/BaseModal.vue
+++ src/UI/components/BaseModal/BaseModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from 'vue'
+import { ref, defineEmits } from 'vue'
 
 interface ModalProps {
   title: string
--- src/UI/components/BaseTable/BaseTable.vue
+++ src/UI/components/BaseTable/BaseTable.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
-
 export interface Column {
   header: string
   key: string
