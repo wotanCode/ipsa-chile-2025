401fd441c71feeda03ee8244627cbc1bc6d4d105
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-25 02:05:42 -0300

    update: unit test

--- src/UI/modules/NavBar/NavBar.spec.ts
+++ src/UI/modules/NavBar/NavBar.spec.ts
@@ -3,7 +3,13 @@ import { mount } from '@vue/test-utils'
 import { createI18n } from 'vue-i18n'
 import { createRouter, createWebHistory } from 'vue-router'
 
-import { HomeIcon, UserIcon, InformationCircleIcon } from '@heroicons/vue/24/outline'
+import {
+  HomeIcon,
+  UserIcon,
+  InformationCircleIcon,
+  FlagIcon,
+  DocumentTextIcon,
+} from '@heroicons/vue/24/outline'
 import NavButton from '@/UI/components/NavButton/NavButton.vue'
 
 import NavBar from './NavBar.vue'
@@ -12,6 +18,8 @@ const messages = {
     navbar: {
       welcome: 'Bienvenido',
       sellers: 'Vendedores',
+      imageRace: 'Carerra de imágenes',
+      invoice: 'Factura',
       about: 'Acerca de',
     },
   },
@@ -27,6 +35,8 @@ const router = createRouter({
   routes: [
     { path: '/', component: {} },
     { path: '/vendedores', component: {} },
+    { path: '/carrera-imagenes', component: {} },
+    { path: '/factura', component: {} },
     { path: '/acerca-de', component: {} },
   ],
 })
@@ -41,10 +51,12 @@ describe('Navbar.vue', () => {
 
     const menuItems = wrapper.findAllComponents(NavButton)
 
-    expect(menuItems.length).toBe(3)
+    expect(menuItems.length).toBe(5)
     expect(menuItems[0].props().text).toBe('Bienvenido')
     expect(menuItems[1].props().text).toBe('Vendedores')
-    expect(menuItems[2].props().text).toBe('Acerca de')
+    expect(menuItems[2].props().text).toBe('Carerra de imágenes')
+    expect(menuItems[3].props().text).toBe('Factura')
+    expect(menuItems[4].props().text).toBe('Acerca de')
   })
 
   test('renders correct icons for each menu item', async () => {
@@ -58,7 +70,9 @@ describe('Navbar.vue', () => {
 
     expect(menuItems[0].props().icon).toBe(HomeIcon)
     expect(menuItems[1].props().icon).toBe(UserIcon)
-    expect(menuItems[2].props().icon).toBe(InformationCircleIcon)
+    expect(menuItems[2].props().icon).toBe(FlagIcon)
+    expect(menuItems[3].props().icon).toBe(DocumentTextIcon)
+    expect(menuItems[4].props().icon).toBe(InformationCircleIcon)
   })
 
   test('renders navigation buttons with correct classes', async () => {
