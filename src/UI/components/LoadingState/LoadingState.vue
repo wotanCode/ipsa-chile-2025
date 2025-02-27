e26f9e23e8bca57212db7cb0b513526b3d3daa26
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-27 12:48:08 -0300

    feat: LoadingState Component

---
+++ src/UI/components/LoadingState/LoadingState.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Component } from 'vue'
+
+interface LoadingStateProps {
+  icon?: Component
+  message?: string
+}
+
+const props = defineProps<LoadingStateProps>()
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center p-4">
+    <component :is="props.icon" class="h-16 w-16 animate-spin text-[var(--color-primary)]" />
+    <p v-if="props.message" class="mt-4 text-lg">{{ props.message }}</p>
+  </div>
+</template>
