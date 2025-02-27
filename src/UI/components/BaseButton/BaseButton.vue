8c136e7ce7ccffc9113facfab9ea088ae4b1fa1c
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-27 12:46:59 -0300

    Feat: add BaseButton Component

---
+++ src/UI/components/BaseButton/BaseButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface ButtonProps {
+  onClick?: () => void
+  disabled?: boolean
+  label: string
+}
+
+const props = defineProps<ButtonProps>()
+</script>
+
+<template>
+  <button
+    class="px-6 py-2.5 bg-[var(--color-primary)] text-white rounded-lg hover:bg-[var(--color-primary)]/90 active:bg-[var(--color-primary)]/90 disabled:opacity-70 disabled:cursor-not-allowed transition-colors"
+    @click="props.onClick"
+    :disabled="props.disabled"
+  >
+    {{ props.label }}
+  </button>
+</template>
