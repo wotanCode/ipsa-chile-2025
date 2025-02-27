ea5e7961ab1a44c700875ca48fd7afa5ffdecbb9
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-27 12:47:57 -0300

    feat: BaseButton Component

--- src/UI/components/BaseButton/BaseButton.vue
+++ src/UI/components/BaseButton/BaseButton.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { type Component } from 'vue'
 
 interface ButtonProps {
   onClick?: () => void
   disabled?: boolean
   label: string
+  icon?: Component
 }
 
 const props = defineProps<ButtonProps>()
@@ -12,10 +13,11 @@ const props = defineProps<ButtonProps>()
 
 <template>
   <button
-    class="px-6 py-2.5 bg-[var(--color-primary)] text-white rounded-lg hover:bg-[var(--color-primary)]/90 active:bg-[var(--color-primary)]/90 disabled:opacity-70 disabled:cursor-not-allowed transition-colors"
+    class="flex items-center px-6 py-2.5 bg-[var(--color-primary)] text-white rounded-lg hover:bg-[var(--color-primary)]/90 active:bg-[var(--color-primary)]/90 disabled:opacity-70 disabled:cursor-not-allowed transition-colors"
     @click="props.onClick"
     :disabled="props.disabled"
   >
+    <component v-if="props.icon" :is="props.icon" class="h-5 w-5 mr-2" />
     {{ props.label }}
   </button>
 </template>
