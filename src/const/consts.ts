5a1ab298e16278ed462999bf98a7cbb01953fa19
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-27 12:47:09 -0300

    add: more consts

--- src/const/consts.ts
+++ src/const/consts.ts
@@ -1,1 +1,8 @@
-export const IMAGES_PER_SELLER = 7
+export const IMAGES_PER_SELLER = 1
+
+export const SCORE_TO_WIN = 20
+
+export const MIN_SELLERS_POSSIBLE = 2
+export const MAX_SELLERS_POSSIBLE = 30
+
+export const WINNER_IMAGE_POINTS = 3
