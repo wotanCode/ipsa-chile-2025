2e5502042c11a354c450c553e58fffb7fbd15345
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-02-27 14:41:29 -0300

    Update: routes const

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
+export const routes = {
+  home: '/',
+  sellers: '/vendedores',
+  imageRace: '/carrera-imagenes',
+  invoice: '/factura',
+  about: '/acerca-de',
+  sellerDetail: '/vendedor/:sellerId',
+  invoiceDetail: '/invoice/:invoiceId',
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: routes.home,
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/vendedores',
+      path: routes.sellers,
       name: 'sellers',
       component: () => import('@/views/SellersView.vue'),
     },
     {
-      path: '/carrera-imagenes',
+      path: routes.imageRace,
       name: 'imageRace',
       component: () => import('@/views/ImageRaceView.vue'),
     },
     {
-      path: '/factura',
+      path: routes.invoice,
       name: 'invoice',
       component: () => import('@/views/InvoiceView.vue'),
     },
     {
-      path: '/acerca-de',
+      path: routes.about,
       name: 'about',
       component: () => import('@/views/AboutView.vue'),
     },
     {
-      path: '/vendedor/:sellerId',
+      path: routes.sellerDetail,
       name: 'sellerDetail',
       component: () => import('@/views/SellerDetailView.vue'),
     },
     {
-      path: '/invoice/:invoiceId',
+      path: routes.invoiceDetail,
       name: 'InvoiceDetail',
       component: () => import('@/views/InvoiceDetailView.vue'),
       props: true,
